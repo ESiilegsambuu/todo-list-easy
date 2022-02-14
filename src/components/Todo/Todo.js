e41fe71591799684537f95b3bcc21e614d543e57
leonvallejos <dev.leonvallejos@gmail.com>
2022-02-14 00:03:43 -0300

    Add handleOnChange and handleOnSubmit

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import uuidv4 from "uuid/v4";
 import './Todo.css';
 
 class Todo extends React.Component {
@@ -10,14 +11,75 @@ class Todo extends React.Component {
         }
     }
 
+    componentWillMount() {
+        this.setState({
+            items: [
+                {
+                    id: uuidv4(),
+                    task: 'Pay the rent',
+                    completed: false
+                },
+                {
+                    id: uuidv4(),
+                    task: 'Go to the gym',
+                    completed: false
+                },
+                {
+                    id: uuidv4(),
+                    task: 'Do my homework',
+                    completed: false
+                }
+            ]
+        });
+    }
+
+    handleOnChange = e => {
+        const {target: {value}} = e;
+        this.setState({
+            task: value
+        });
+    }
+
+    handleOnSubmit = e => {
+        e.preventDefault();
+        if (this.state.task.trim() === '') {
+            this.setState({
+                task: '',
+                items: [
+                    ...this.state.items,
+                    {
+                        id: uuidv4(),
+                        task: this.state.task,
+                        completed: false
+                    }
+                ]
+            });
+        }
+    }
+
+    markAsCompleted = id => {
+        
+    }
+
 
     render() {
         return (
             <div className="Todo">
                 <h1>New Task:</h1>
+
+                <form onSubmit={this.handleOnSubmit}>
+                    <input value={this.state.task} onChange={this.handleOnChange}/>
+                </form>
+
+                <List
+                    items={this.state.items}
+                    markAsCompleted={this.markAsCompleted}
+                    removeTask={this.removeTask}
+                />
             </div>
         )
     }
 }
 
 export default Todo;
+    
